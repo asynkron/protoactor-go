e4fe60f58213f76514b3c2742352067b0e1c07bd
rogeralsing <rogeralsing@gmail.com>
2016-04-19 19:53:53 +0200

    ...

--- supervision.go
+++ supervision.go
@@ -1,5 +1,19 @@
 package actor
 
+type Directive int
+const (
+    ResumeDirective Directive = iota
+    RestartDirective
+    StopDirective
+    EscalateDirective
+)
+
+type Decider func(child ActorRef, cause interface{}) Directive
+
+type SupervisionStrategy interface {
+    Handle(child ActorRef, cause interface{}) Directive
+}
+
 type OneForOneStrategy struct {
 	maxNrOfRetries              int
 	withinTimeRangeMilliseconds int
