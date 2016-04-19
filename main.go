18145be505565af6a39af9f52988c5b88d316953
rogeralsing <rogeralsing@gmail.com>
2016-04-19 16:44:37 +0200

    hello world

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 import "github.com/rogeralsing/goactor/actor"
 
 func main() {
-	// decider := func(child actor.ActorRef, reason interface{}) actor.Directive {
-	// 	fmt.Println("restarting failing child")
-	// 	return actor.Restart
-	// }
-
 	props := actor.
 		Props(NewParentActor).
 		WithMailbox(actor.NewUnboundedMailbox()).
@@ -26,10 +21,7 @@ func main() {
 	reader.ReadString('\n')
 }
 
-type Ping struct {
-	Sender actor.ActorRef
-	Name   string
-}
+type Ping struct{ Sender actor.ActorRef }
 type Pong struct{}
 type Hello struct{ Name string }
 
@@ -41,15 +33,7 @@ func NewChildActor() actor.Actor {
 
 func (state *ChildActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
-	case actor.Starting:
-		fmt.Println("Im starting")
-	case actor.Stopping:
-		fmt.Println("stopping child")
-	case actor.Stopped:
-		fmt.Println("stopped child")
 	case Ping:
-		fmt.Printf("Hello %v\n", msg.Name)
-		panic("hej")
 		state.messageCount++
 		fmt.Printf("message count %v \n", state.messageCount)
 		msg.Sender.Tell(Pong{})
@@ -68,30 +52,8 @@ func (state *ParentActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
 	case actor.Starting:
 		state.Child = context.SpawnChild(actor.Props(NewChildActor))
-	case actor.Stopping:
-		fmt.Println("stopping parent")
-	case actor.Stopped:
-		fmt.Println("stopped parent")
-
 	case Hello:
 		fmt.Printf("Parent got hello %v\n", msg.Name)
-		state.Child.Tell(Ping{
-			Name:   msg.Name,
-			Sender: context.Self(),
-		})
-		context.Become(state.Other)
-	}
-}
-
-func (state *ParentActor) Other(context actor.Context) {
-	switch context.Message().(type) {
-	case actor.Stopping:
-		fmt.Println("stopping parent in become")
-	case actor.Stopped:
-		fmt.Println("stopped parent in become")
-
-	case Pong:
-		fmt.Println("Got pong")
-		context.Self().Stop()
+		state.Child.Tell(Ping{Sender: context.Self()})
 	}
 }
