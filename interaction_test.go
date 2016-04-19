d851fe55eed37f0074e97d0e3960b8f131bcd32d
rogeralsing <rogeralsing@gmail.com>
2016-04-19 22:28:01 +0200

    more tests

--- interaction_test.go
+++ interaction_test.go
@@ -20,6 +20,7 @@ func NewBlackHoleActor() Actor {
 
 func TestActorOfProducesActorRef(t *testing.T) {
 	actor := ActorOf(Props(NewBlackHoleActor))
+    defer actor.Stop()
 	assert.NotNil(t, actor)
 }
 
@@ -41,6 +42,7 @@ func (state *FuncActor) Receive(context Context) {
 func TestActorReceivesMessage(t *testing.T) {
 	done := make(chan struct{})
 	actor := ActorOf(Props(NewFuncActor(func() { close(done) })))
+    defer actor.Stop()
 	actor.Tell(DummyMessage{})
 	select {
 	case <-done:
@@ -69,6 +71,7 @@ func (EchoActor) Receive(context Context) {
 func TestActorCanReplyToMessage(t *testing.T) {
 	future := NewFutureActorRef()
 	actor := ActorOf(Props(NewEchoActor))
+    defer actor.Stop()
 	actor.Tell(EchoRequest{Sender: future})
 	select {
 	case <-future.Result():
@@ -102,6 +105,7 @@ func (EchoBecomeActor) Other(context Context) {
 func TestActorCanBecome(t *testing.T) {
 	future := NewFutureActorRef()
 	actor := ActorOf(Props(NewEchoActor))
+    defer actor.Stop()
 	actor.Tell(BecomeRequest{})
 	actor.Tell(EchoRequest{Sender: future})
 	select {
@@ -138,9 +142,63 @@ func (EchoUnbecomeActor) Other(context Context) {
 func TestActorCanUnbecome(t *testing.T) {
 	future := NewFutureActorRef()
 	actor := ActorOf(Props(NewEchoActor))
+    defer actor.Stop()
 	actor.Tell(BecomeRequest{})
 	actor.Tell(UnbecomeRequest{})
 	actor.Tell(EchoRequest{Sender: future})
+	select {
+	case <-future.Result():
+	case <-time.After(testTimeout):
+		assert.Fail(t, "timed out")
+	}
+}
+
+type EchoOnStartActor struct{replyTo ActorRef}
+
+func (state EchoOnStartActor) Receive(context Context) {
+	switch context.Message().(type) {
+	case Starting:
+		state.replyTo.Tell(EchoResponse{})
+	}
+}
+
+func NewEchoOnStartActor(replyTo ActorRef) func() Actor {
+    return func() Actor{
+	    return &EchoOnStartActor{replyTo: replyTo}
+    }
+}
+
+func TestActorCanReplyOnStarting(t *testing.T) {
+	future := NewFutureActorRef()
+	actor := ActorOf(Props(NewEchoOnStartActor(future)))
+    defer actor.Stop()
+	select {
+	case <-future.Result():
+	case <-time.After(testTimeout):
+		assert.Fail(t, "timed out")
+	}
+}
+
+
+type EchoOnStoppingActor struct{replyTo ActorRef}
+
+func (state EchoOnStoppingActor) Receive(context Context) {
+	switch context.Message().(type) {
+	case Stopping:
+		state.replyTo.Tell(EchoResponse{})
+	}
+}
+
+func NewEchoOnStoppingActor(replyTo ActorRef) func() Actor {
+    return func() Actor{
+	    return &EchoOnStoppingActor{replyTo: replyTo}
+    }
+}
+
+func TestActorCanReplyOnStopping(t *testing.T) {
+	future := NewFutureActorRef()
+	actor := ActorOf(Props(NewEchoOnStoppingActor(future)))
+    actor.Stop()
 	select {
 	case <-future.Result():
 	case <-time.After(testTimeout):
