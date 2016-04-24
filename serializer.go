78517be26e202f25bae0ef15568c48ecde2e40b4
rogeralsing <rogeralsing@gmail.com>
2016-04-24 16:39:43 +0200

    serializer

--- serializer.go
+++ serializer.go
@@ -1,8 +1,9 @@
 package gam
 
+import "reflect"
 import proto "github.com/golang/protobuf/proto"
 
-func NewMessageEnvelope(message proto.Message) (*MessageEnvelope, error) {
+func PackMessage(message proto.Message) (*MessageEnvelope, error) {
 	typeName := proto.MessageName(message)
 	bytes, err := proto.Marshal(message)
 	if err != nil {
@@ -14,4 +15,13 @@ func NewMessageEnvelope(message proto.Message) (*MessageEnvelope, error) {
 	}
 
 	return envelope, nil
+}
+
+func UnpackMessage(message *MessageEnvelope) proto.Message {
+	buf := message.MessageData
+	t := proto.MessageType(message.TypeName).Elem()
+	intPtr := reflect.New(t)
+	instance := intPtr.Interface().(proto.Message)
+	proto.Unmarshal(buf, instance)
+	return instance
 }
