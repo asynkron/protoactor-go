72492e40a0e846b6de43f7b821d184ed864fd840
rogeralsing <rogeralsing@gmail.com>
2016-04-26 20:32:19 +0200

    protos

--- examples/remoting/messages/buildproto.bat
+++ examples/remoting/messages/buildproto.bat
@@ -1,1 +1,1 @@
-c:\proto\protoc --go_out=Mactor.proto=github.com/rogeralsing/gam/actor:. --proto_path=. --proto_path=c:\Projects\Go\src\github.com\rogeralsing\gam\actor messages.proto
+c:\proto\protoc --go_out=. --proto_path=.  --proto_path=c:\Projects\Go\src messages.proto
