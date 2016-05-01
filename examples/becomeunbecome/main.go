3925ccdc51f98c567bd7d887d1678a8d077fbac9
rogeralsing <rogeralsing@gmail.com>
2016-05-01 15:11:53 +0200

    console

--- examples/becomeunbecome/main.go
+++ examples/becomeunbecome/main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/rogeralsing/gam/actor"
-    "fmt"
-	"os"
-    "bufio"
+	"github.com/rogeralsing/goconsole"
 )
 
-type Become struct {}
+type Become struct{}
 type Hello struct{ Who string }
 type BecomeActor struct{}
 
 func (state *BecomeActor) Receive(context actor.Context) {
-    switch msg := context.Message().(type) {
-    case Hello:
-        fmt.Printf("Hello %v\n", msg.Who)
-        context.Become(state.Other)
-    }
+	switch msg := context.Message().(type) {
+	case Hello:
+		fmt.Printf("Hello %v\n", msg.Who)
+		context.Become(state.Other)
+	}
 }
 
 func (state *BecomeActor) Other(context actor.Context) {
-    switch msg := context.Message().(type) {
-    case Hello:
-        fmt.Printf("%v, ey we are now handling messages in another behavior",msg.Who)
-    }
+	switch msg := context.Message().(type) {
+	case Hello:
+		fmt.Printf("%v, ey we are now handling messages in another behavior", msg.Who)
+	}
 }
 
 func NewBecomeActor() actor.Actor {
-    return &BecomeActor{}
+	return &BecomeActor{}
 }
 
 func main() {
-    pid := actor.Spawn(actor.Props(NewBecomeActor))
-    pid.Tell(Hello{Who: "Roger"})
-    pid.Tell(Hello{Who: "Roger"})
-    bufio.NewReader(os.Stdin).ReadString('\n')
+	pid := actor.Spawn(actor.Props(NewBecomeActor))
+	pid.Tell(Hello{Who: "Roger"})
+	pid.Tell(Hello{Who: "Roger"})
+	console.ReadLine()
 }
