05de10cc1ba500ea8bc120788d986e115e4940be
rogeralsing <rogeralsing@gmail.com>
2016-05-01 22:25:05 +0200

    renamed props

--- actor/props.go
+++ actor/props.go
@@ -6,50 +6,50 @@ type Properties interface {
 	Supervisor() SupervisionStrategy
 }
 
-type PropsValue struct {
+type Props struct {
 	actorProducer       ActorProducer
 	mailboxProducer     MailboxProducer
 	supervisionStrategy SupervisionStrategy
 }
 
-func (props PropsValue) ProduceActor() Actor {
+func (props Props) ProduceActor() Actor {
 	return props.actorProducer()
 }
 
-func (props PropsValue) Supervisor() SupervisionStrategy {
+func (props Props) Supervisor() SupervisionStrategy {
 	if props.supervisionStrategy == nil {
 		return DefaultSupervisionStrategy()
 	}
 	return props.supervisionStrategy
 }
 
-func (props PropsValue) ProduceMailbox() Mailbox {
+func (props Props) ProduceMailbox() Mailbox {
 	if props.mailboxProducer == nil {
 		return NewUnboundedMailbox(100)()
 	}
 	return props.mailboxProducer()
 }
 
-func (props PropsValue) WithMailbox(mailbox MailboxProducer) PropsValue {
+func (props Props) WithMailbox(mailbox MailboxProducer) Props {
 	//pass by value, we only modify the copy
 	props.mailboxProducer = mailbox
 	return props
 }
 
-func (props PropsValue) WithSupervisor(supervisor SupervisionStrategy) PropsValue {
+func (props Props) WithSupervisor(supervisor SupervisionStrategy) Props {
 	//pass by value, we only modify the copy
 	props.supervisionStrategy = supervisor
 	return props
 }
 
-func FromProducer(actorProducer ActorProducer) PropsValue {
-	return PropsValue{
+func FromProducer(actorProducer ActorProducer) Props {
+	return Props{
 		actorProducer:   actorProducer,
 		mailboxProducer: nil,
 	}
 }
 
-func FromFunc(receive Receive) PropsValue {
+func FromFunc(receive Receive) Props {
 	a := &EmptyActor{
 		receive: receive,
 	}
@@ -59,7 +59,7 @@ func FromFunc(receive Receive) PropsValue {
 	return p
 }
 
-func FromInstance(template Actor) PropsValue {
+func FromInstance(template Actor) Props {
 	producer := func() Actor {
 		return template
 	}
