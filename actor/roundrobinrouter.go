96a2c7975a88ace23f7686d2aa0a4132cab5ffca
rogeralsing <rogeralsing@gmail.com>
2016-05-03 06:47:03 +0200

    Routers

---
+++ actor/roundrobinrouter.go
@@ -0,0 +1,74 @@
+package actor
+
+import "sync/atomic"
+
+type RouterState interface {
+	Route(message interface{})
+	SetRoutees(routees []*PID)
+}
+
+type RoundRobinGroupRouter struct {
+	routees []*PID
+}
+
+type RoundRobinPoolRouter struct {
+	poolSize int
+}
+
+type RoundRobinState struct {
+	index   int32
+	routees []*PID
+	config  RouterConfig
+}
+
+func (state *RoundRobinState) SetRoutees(routees []*PID) {
+	state.routees = routees
+}
+
+func NewRoundRobinGroupRouter(routees ...*PID) RouterConfig {
+	return &RoundRobinGroupRouter{routees: routees}
+}
+
+func NewRoundRobinPoolRouter(poolSize int) RouterConfig {
+	return &RoundRobinPoolRouter{poolSize: poolSize}
+}
+
+func (state *RoundRobinState) Route(message interface{}) {
+	pid := roundRobinRoutee(&state.index, state.routees)
+	pid.Tell(message)
+}
+
+func (config *RoundRobinPoolRouter) Create() RouterState {
+	return &RoundRobinState{
+		config: config,
+	}
+}
+
+func (config *RoundRobinGroupRouter) Create() RouterState {
+	return &RoundRobinState{
+		config: config,
+	}
+}
+
+func roundRobinRoutee(index *int32, routees []*PID) *PID {
+	i := int(atomic.AddInt32(index, 1))
+	mod := len(routees)
+	routee := routees[i%mod]
+	return routee
+}
+
+func (config *RoundRobinGroupRouter) OnStarted(context Context, props Props, router RouterState) {
+	for _, r := range config.routees {
+		context.Watch(r)
+	}
+	router.SetRoutees(config.routees)
+}
+
+func (config *RoundRobinPoolRouter) OnStarted(context Context, props Props, router RouterState) {
+	routees := make([]*PID, config.poolSize)
+	for i := 0; i < config.poolSize; i++ {
+		pid := context.Spawn(props)
+		routees[i] = pid
+	}
+	router.SetRoutees(routees)
+}
--- actor/router.go
+++ actor/router.go
@@ -1,84 +1,9 @@
 package actor
 
-import "sync/atomic"
-
 type RouterConfig interface {
 	OnStarted(context Context, props Props, router RouterState)
 
 	Create() RouterState
-}
-
-type RoundRobinGroupRouter struct {
-	index   int32
-	routees []*PID
-}
-
-type RoundRobinPoolRouter struct {
-	index    int32
-	poolSize int
-}
-
-type RouterState interface {
-	Route(message interface{})
-	SetRoutees(routees []*PID)
-}
-
-type RoundRobinState struct {
-	index   int32
-	routees []*PID
-	config  RouterConfig
-}
-
-func (state *RoundRobinState) SetRoutees(routees []*PID) {
-	state.routees = routees
-}
-
-func NewRoundRobinGroupRouter(routees ...*PID) RouterConfig {
-	return &RoundRobinGroupRouter{routees: routees}
-}
-
-func NewRoundRobinPoolRouter(poolSize int) RouterConfig {
-	return &RoundRobinPoolRouter{poolSize: poolSize}
-}
-
-func (state *RoundRobinState) Route(message interface{}) {
-	pid := roundRobinRoutee(&state.index, state.routees)
-	pid.Tell(message)
-}
-
-func (config *RoundRobinPoolRouter) Create() RouterState {
-	return &RoundRobinState{
-		config: config,
-	}
-}
-
-func (config *RoundRobinGroupRouter) Create() RouterState {
-	return &RoundRobinState{
-		config: config,
-	}
-}
-
-func roundRobinRoutee(index *int32, routees []*PID) *PID {
-	i := int(atomic.AddInt32(index, 1))
-	mod := len(routees)
-	routee := routees[i%mod]
-	return routee
-}
-
-func (config *RoundRobinGroupRouter) OnStarted(context Context, props Props, router RouterState) {
-	for _, r := range config.routees {
-		context.Watch(r)
-	}
-	router.SetRoutees(config.routees)
-}
-
-func (config *RoundRobinPoolRouter) OnStarted(context Context, props Props, router RouterState) {
-	routees := make([]*PID, config.poolSize)
-	for i := 0; i < config.poolSize; i++ {
-		pid := context.Spawn(props)
-		routees[i] = pid
-	}
-	router.SetRoutees(routees)
 }
 
 var NoRouter RouterConfig = nil
@@ -95,7 +20,11 @@ func spawnRouter(config RouterConfig, props Props, parent *PID) *PID {
 		}
 	})
 	router := spawn(id, routerProps, parent)
-	ref := newRouterActorRef(router, routerState)
+
+	ref := &RouterActorRef{
+		router: router,
+		state:  routerState,
+	}
 	proxyID := ProcessRegistry.getAutoId()
 	proxy := ProcessRegistry.registerPID(ref, proxyID)
 	return proxy
@@ -109,11 +38,4 @@ type RouterActorRef struct {
 
 func (ref *RouterActorRef) Tell(message interface{}) {
 	ref.state.Route(message)
-}
-
-func newRouterActorRef(router *PID, state RouterState) ActorRef {
-	return &RouterActorRef{
-		router: router,
-		state:  state,
-	}
 }
