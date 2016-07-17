7c17988866a1bc3adb7515cb90c4ee3f43d8bcf2
Laurent Charignon <l.charignon@gmail.com>
2016-07-17 15:27:53 -0700

    Add RandomPoolRouter and RandomGroupRouter

--- examples/routing/main.go
+++ examples/routing/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/rogeralsing/gam/actor"
 	"github.com/rogeralsing/goconsole"
@@ -16,11 +17,18 @@ func main() {
 			log.Printf("%v got message %d", context.Self(), context.Message().(myMessage).i)
 		}
 	}
+	log.Println("Round robin routing:")
 	props := actor.FromFunc(act).WithPoolRouter(actor.NewRoundRobinPool(10))
 	pid := actor.Spawn(props)
 	for i := 0; i < 10; i++ {
 		pid.Tell(myMessage{i})
 	}
-
+	time.Sleep(1 * time.Second)
+	log.Println("Random routing:")
+	props = actor.FromFunc(act).WithPoolRouter(actor.NewRandomPool(10))
+	pid = actor.Spawn(props)
+	for i := 0; i < 10; i++ {
+		pid.Tell(myMessage{i})
+	}
 	console.ReadLine()
 }
