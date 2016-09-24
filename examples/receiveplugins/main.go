9e66421774a0a09199b4ae0c9d39db2809691557
rogeralsing <rogeralsing@gmail.com>
2016-09-24 11:49:03 +0200

    formatting

--- examples/receiveplugins/main.go
+++ examples/receiveplugins/main.go
@@ -17,12 +17,22 @@ func (state *HelloActor) Receive(context actor.Context) {
 	}
 }
 
+func myReceivePlugin(context actor.Context) interface{} {
+	message := context.Message()
+	fmt.Printf("Received message %v\n", message)
+
+	switch msg := context.Message().(type) {
+	case Hello:
+		return Hello{
+			Who: msg.Who + " Modified",
+		}
+	}
+
+	return message
+}
+
 func main() {
-	props := actor.FromInstance(&HelloActor{}).WithReceivePlugin(func(context actor.Context) interface{} {
-		message := context.Message()
-		fmt.Printf("Received message %v\n", message)
-		return message
-	})
+	props := actor.FromInstance(&HelloActor{}).WithReceivePlugin(myReceivePlugin)
 	pid := actor.Spawn(props)
 	pid.Tell(Hello{Who: "Roger"})
 	console.ReadLine()
