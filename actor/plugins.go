b068ddc46ae256cfe46a7bb2764718a37143e0b8
rogeralsing <rogeralsing@gmail.com>
2016-09-24 23:02:00 +0200

    better built in logging

--- actor/plugins.go
+++ actor/plugins.go
@@ -1,9 +1,12 @@
 package actor
 
-import "log"
+import (
+	"log"
+	"reflect"
+)
 
 func ConsoleLogging(context Context) {
 	message := context.Message()
-	log.Printf("%v got %+v", context.Self(), message)
+	log.Printf("%v got %v %+v", context.Self(), reflect.TypeOf(message), message)
 	context.Handle(message)
 }
