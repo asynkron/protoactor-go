a05ce3fdeb2bc1220478feef6f1934df363e9864
rogeralsing <rogeralsing@gmail.com>
2016-09-30 07:13:03 +0200

    snapshots works

--- couchbase_persistence/couchbase_provider.go
+++ couchbase_persistence/couchbase_provider.go
@@ -33,17 +33,17 @@ func New(bucketName string, baseU string) *Provider {
 }
 
 func formatEventKey(actorName string, eventIndex int) string {
-	key := fmt.Sprintf("%v-%010devent", actorName, eventIndex)
+	key := fmt.Sprintf("%v-event-%010d", actorName, eventIndex)
 	return key
 }
 
 func formatSnapshotKey(actorName string, eventIndex int) string {
-	key := fmt.Sprintf("%v-%010dsnapshot", actorName, eventIndex)
+	key := fmt.Sprintf("%v-snapshot-%010d", actorName, eventIndex)
 	return key
 }
 
 func (provider *Provider) GetEvents(actorName string, eventIndexStart int, callback func(event interface{})) {
-	q := gocb.NewN1qlQuery("SELECT b.* FROM `" + provider.bucketName + "` b WHERE meta(b).id >= $1 and meta(b).id <= $2")
+	q := gocb.NewN1qlQuery("SELECT b.* FROM `" + provider.bucketName + "` b WHERE meta(b).id >= $1 and meta(b).id <= $2 and snapshot=false")
 	var p []interface{}
 	p = append(p, formatEventKey(actorName, eventIndexStart))
 	p = append(p, formatEventKey(actorName, 9999999999))
@@ -62,7 +62,7 @@ func (provider *Provider) GetEvents(actorName string, eventIndexStart int, callb
 }
 
 func (provider *Provider) GetSnapshot(actorName string) (snapshot interface{}, eventIndex int, ok bool) {
-	q := gocb.NewN1qlQuery("SELECT b.* FROM `" + provider.bucketName + "` b WHERE meta(b).id >= $1 and meta(b).id <= $2 order by b.eventIndex desc limit 1")
+	q := gocb.NewN1qlQuery("SELECT b.* FROM `" + provider.bucketName + "` b WHERE meta(b).id >= $1 and meta(b).id <= $2 and snapshot=true order by b.eventIndex desc limit 1")
 	var p []interface{}
 	p = append(p, formatSnapshotKey(actorName, 0))
 	p = append(p, formatSnapshotKey(actorName, 9999999999))
@@ -133,7 +133,7 @@ type Envelope struct {
 	Type       string          `json:"type"`
 	Message    json.RawMessage `json:"event"`
 	EventIndex int             `json:"eventIndex"`
-	Snapshot   bool            `json:"snapshot`
+	Snapshot   bool            `json:"snapshot"`
 }
 
 type Transcoder struct {
