7072bbe6fb4228c7abb5ba1e4e48820ea5f15007
dtravin <dtravin@gmail.com>
2016-11-17 10:34:10 +0200

    function return order now follows Go convention

--- actor/pid.go
+++ actor/pid.go
@@ -7,14 +7,14 @@ func (pid *PID) Tell(message interface{}) {
 	ref.Tell(message)
 }
 
-func (pid *PID) Ask(message interface{}) (error, *Response) {
+func (pid *PID) Ask(message interface{}) (*Response, error) {
 	ref, found := ProcessRegistry.fromPID(pid)
 	if !found {
-		return fmt.Errorf("Unknown PID %s", pid), nil
+		return nil, fmt.Errorf("Unknown PID %s", pid)
 	}
 	pid, result := RequestResponsePID()
 	ref.Ask(message, pid)
-	return nil, result
+	return result, nil
 }
 
 func (pid *PID) sendSystemMessage(message SystemMessage) {
