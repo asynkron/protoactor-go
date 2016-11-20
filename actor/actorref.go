3b3fddb117013593065d399862ddc1ef6047ea00
dtravin <dtravin@gmail.com>
2016-11-20 04:18:05 +0200

    #27 PID.StopFuture() implemented

--- actor/actorref.go
+++ actor/actorref.go
@@ -6,6 +6,8 @@ type ActorRef interface {
 	Ask(pid *PID, message interface{}, sender *PID)
 	SendSystemMessage(pid *PID, message SystemMessage)
 	Stop(pid *PID)
+	Watch(pid *PID)
+	UnWatch(pid *PID)
 }
 
 type LocalActorRef struct {
@@ -32,6 +34,14 @@ func (ref *LocalActorRef) SendSystemMessage(pid *PID, message SystemMessage) {
 
 func (ref *LocalActorRef) Stop(pid *PID) {
 	ref.SendSystemMessage(pid, &stop{})
+}
+
+func (ref *LocalActorRef) Watch(pid *PID) {
+	ref.SendSystemMessage(pid, &watch{Watcher: pid})
+}
+
+func (ref *LocalActorRef) UnWatch(pid *PID) {
+	ref.SendSystemMessage(pid, &unwatch{Watcher: pid})
 }
 
 func (ref *LocalActorRef) Suspend() {
@@ -70,6 +80,12 @@ func (DeadLetterActorRef) SendSystemMessage(pid *PID, message SystemMessage) {
 }
 
 func (DeadLetterActorRef) Stop(pid *PID) {
+}
+
+func (DeadLetterActorRef) Watch(pid *PID) {
+}
+
+func (DeadLetterActorRef) UnWatch(pid *PID) {
 }
 
 //log.Printf("Deadletter for %v got %+v", pid, message)
--- actor/pid.go
+++ actor/pid.go
@@ -3,6 +3,8 @@ package actor
 import (
 	"fmt"
 	"time"
+	"log"
+	"reflect"
 )
 
 //Tell a message to a given PID
@@ -35,6 +37,27 @@ func (pid *PID) AskFuture(message interface{}, timeout time.Duration) (*Future, 
 func (pid *PID) sendSystemMessage(message SystemMessage) {
 	ref, _ := ProcessRegistry.fromPID(pid)
 	ref.SendSystemMessage(pid, message)
+}
+
+func (pid *PID) StopFuture() (*Future, error) {
+	ref, found := ProcessRegistry.fromPID(pid)
+
+	if !found {
+		return nil, fmt.Errorf("Unknown PID %s", pid)
+	}
+	future := NewFuture(10 * time.Second)
+
+	log.Printf("ref = %s", reflect.TypeOf(ref))
+	ref, ok := ref.(*LocalActorRef)
+	if !ok {
+		log.Fatalf("Ooops %s", reflect.TypeOf(ref))
+	}
+
+	ref.Watch(future.PID())
+
+	ref.Stop(pid)
+
+	return future, nil
 }
 
 //Stop the given PID
