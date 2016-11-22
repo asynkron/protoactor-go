88b063e8a3d70672108ef016cf594e33cf42523a
Saman Barghi <saman.b@gmail.com>
2016-11-22 01:25:40 -0500

    Replaced hashmap + rwlock with ctrie in ProcessRegistry (#29)
    * ProcessRegistery: replaced hashmap with ctrie
    
    Replaced hashmap+RWLock with ctrie for better performance.
    
    * ProcessRegister: fixed small issues with the code

--- actor/process_registry.go
+++ actor/process_registry.go
@@ -2,23 +2,23 @@ package actor
 
 import (
 	"strconv"
-	"sync"
 	"sync/atomic"
+
+	"github.com/Workiva/go-datastructures/trie/ctrie"
 )
 
 type HostResolver func(*PID) (ActorRef, bool)
 
 type ProcessRegistryValue struct {
 	Host           string
-	LocalPids      map[string]ActorRef //maybe this should be replaced with something lockfree like ctrie instead
+	LocalPids      *ctrie.Ctrie
 	RemoteHandlers []HostResolver
 	SequenceID     uint64
-	rw             sync.RWMutex
 }
 
 var ProcessRegistry = &ProcessRegistryValue{
 	Host:           "nonhost",
-	LocalPids:      make(map[string]ActorRef),
+	LocalPids:      ctrie.New(nil),
 	RemoteHandlers: make([]HostResolver, 0),
 }
 
@@ -38,21 +38,16 @@ func (pr *ProcessRegistryValue) registerPID(actorRef ActorRef, id string) (*PID,
 		Id:   id,
 	}
 
-	pr.rw.Lock()
-	_, found := pr.LocalPids[pid.Id]
+	_, found := pr.LocalPids.Lookup([]byte(pid.Id))
 	if found {
-	    pr.rw.Unlock()
 		return &pid, false
 	}
-	pr.LocalPids[pid.Id] = actorRef
-	pr.rw.Unlock()
+	pr.LocalPids.Insert([]byte(pid.Id), actorRef)
 	return &pid, true
 }
 
 func (pr *ProcessRegistryValue) unregisterPID(pid *PID) {
-	pr.rw.Lock()
-	delete(pr.LocalPids, pid.Id)
-	pr.rw.Unlock()
+	pr.LocalPids.Remove([]byte(pid.Id))
 }
 
 func (pr *ProcessRegistryValue) fromPID(pid *PID) (ActorRef, bool) {
@@ -66,13 +61,16 @@ func (pr *ProcessRegistryValue) fromPID(pid *PID) (ActorRef, bool) {
 		//panic("Unknown host or node")
 		return deadLetter, false
 	}
-	pr.rw.RLock()
-	ref, ok := pr.LocalPids[pid.Id]
+	ref, ok := pr.LocalPids.Lookup([]byte(pid.Id))
 	if !ok {
 		//panic("Unknown PID")
-	    pr.rw.RUnlock()
 		return deadLetter, false
 	}
-	pr.rw.RUnlock()
-	return ref, true
+
+	if original, ok := ref.(ActorRef); ok {
+		return original, true
+	} else {
+		return deadLetter, false
+	}
+
 }
