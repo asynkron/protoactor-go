e1c8381e48b156517e0b572cd98eff1ba1efac59
Pavel Smejkal <pavel.smejkal@gmail.com>
2016-11-23 22:24:10 +0100

    Consisten router and remote group (#32)
    * Consistenhash router, remote group router
    
    * Fixing type related issues
    
    * No logging every message
    
    * Adding readme
    
    * Wrong proto filename

---
+++ actor/consistent_router.go
@@ -0,0 +1,73 @@
+package actor
+
+import (
+	"errors"
+	"log"
+)
+
+var (
+	ErrorUnknownPartition = errors.New("Hasher doesn't return partition")
+)
+
+type Hashable interface {
+	HashBy() string
+}
+
+type Hasher interface {
+	Hash(message Hashable) (string, error)
+	SetNodes(nodes []string)
+}
+
+type ConsistentRouterState struct {
+	routees map[string]*PID
+	hasher  Hasher
+	config  RouterConfig
+}
+
+func NewConsistentRouter(config RouterConfig, hasher Hasher) RouterState {
+	router := &ConsistentRouterState{
+		config: config,
+		hasher: hasher,
+	}
+	return router
+}
+
+func (state *ConsistentRouterState) SetRoutees(routees []*PID) {
+	nodes := make([]string, len(routees))
+	routeesHash := make(map[string]*PID)
+
+	for i, r := range routees {
+		routeesHash[r.Host] = r
+		nodes[i] = r.Host
+	}
+
+	state.hasher.SetNodes(nodes)
+
+	oldRoutees := state.routees
+
+	state.routees = routeesHash
+
+	for _, r := range oldRoutees {
+		r.Stop()
+	}
+}
+
+func (state *ConsistentRouterState) Route(message interface{}) {
+
+	switch msg := message.(type) {
+	case Hashable:
+		node, err := state.hasher.Hash(msg)
+		if err != nil {
+			log.Println("Consisten router failed to derminate node", err)
+			return
+		}
+		if routee, ok := state.routees[node]; ok {
+			routee.Tell(msg)
+		} else {
+			log.Println("Consisten router failed to resolve node", node)
+		}
+	default:
+		log.Println("Unknown message", msg)
+	}
+
+}
