9de5a170391b5559dcf0fff277b6f15c697b4cfa
rogeralsing <rogeralsing@gmail.com>
2016-11-24 15:30:19 +0100

    remote system messages

--- remoting/remote_actorref.go
+++ remoting/remote_actorref.go
@@ -19,13 +19,7 @@ func newRemoteActorRef(pid *actor.PID) actor.ActorRef {
 }
 
 func (ref *remoteActorRef) Tell(pid *actor.PID, message interface{}) {
-	switch msg := message.(type) {
-	case proto.Message:
-		envelope, _ := packMessage(msg, ref.pid, nil)
-		endpointManagerPID.Tell(envelope)
-	default:
-		log.Printf("[REMOTING] failed, trying to send non Proto %v message to %v", msg, ref.pid)
-	}
+	ref.Ask(pid, message, nil)
 }
 
 func (ref *remoteActorRef) Ask(pid *actor.PID, message interface{}, sender *actor.PID) {
@@ -36,4 +30,29 @@ func (ref *remoteActorRef) Ask(pid *actor.PID, message interface{}, sender *acto
 	default:
 		log.Printf("[REMOTING] failed, trying to send non Proto %v message to %v", msg, ref.pid)
 	}
+}
+
+func (ref *remoteActorRef) SendSystemMessage(pid *actor.PID, message actor.SystemMessage) {
+	switch msg := message.(type) {
+	case *actor.Watch:
+		ref.Ask(pid, msg, nil)
+	case *actor.Unwatch:
+		ref.Ask(pid, msg, nil)
+	case *actor.Terminated:
+		ref.Ask(pid, msg, nil)
+	default:
+		log.Printf("[REMOTING] failed, trying to send non Proto %v message to %v", msg, ref.pid)
+	}
+}
+
+func (ref *remoteActorRef) Stop(pid *actor.PID) {
+	ref.SendSystemMessage(pid, &actor.Stop{})
+}
+
+func (ref *remoteActorRef) Watch(pid *actor.PID) {
+	ref.SendSystemMessage(pid, &actor.Watch{Watcher: pid})
+}
+
+func (ref *remoteActorRef) UnWatch(pid *actor.PID) {
+	ref.SendSystemMessage(pid, &actor.Unwatch{Watcher: pid})
 }
