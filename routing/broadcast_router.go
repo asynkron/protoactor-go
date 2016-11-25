f8a8f3354634a9b50d9dd4467c0a229c3d42b611
rogeralsing <rogeralsing@gmail.com>
2016-11-25 22:47:08 +0100

    broadcast router

---
+++ routing/broadcast_router.go
@@ -0,0 +1,45 @@
+package routing
+
+import "github.com/AsynkronIT/gam/actor"
+
+type BroadcastGroupRouter struct {
+	actor.GroupRouter
+}
+
+type BroadcastPoolRouter struct {
+	actor.PoolRouter
+}
+
+type BroadcastRouterState struct {
+	routees []*actor.PID
+}
+
+func (state *BroadcastRouterState) SetRoutees(routees []*actor.PID) {
+	state.routees = routees
+}
+
+func (state *BroadcastRouterState) RouteMessage(message interface{}, sender *actor.PID) {
+	for _, m := range state.routees {
+		m.TellWithSender(message, sender)
+	}
+}
+
+func NewBroadcastPool(poolSize int) actor.PoolRouterConfig {
+	r := &BroadcastPoolRouter{}
+	r.PoolSize = poolSize
+	return r
+}
+
+func NewBroadcastGroup(routees ...*actor.PID) actor.GroupRouterConfig {
+	r := &BroadcastGroupRouter{}
+	r.Routees = routees
+	return r
+}
+
+func (config *BroadcastPoolRouter) CreateRouterState() actor.RouterState {
+	return &BroadcastRouterState{}
+}
+
+func (config *BroadcastGroupRouter) CreateRouterState() actor.RouterState {
+	return &BroadcastRouterState{}
+}
