b95bd6b725c273dddf33dd8dc290fd78d916145c
rogeralsing <rogeralsing@gmail.com>
2016-11-25 17:45:13 +0100

    .

--- examples/routing/main.go
+++ examples/routing/main.go
@@ -17,28 +17,29 @@ func (m *myMessage) HashBy() string {
 }
 
 func main() {
-	act := func(context actor.Context) {
+
+	log.Println("Round robin routing:")
+	act := actor.FromFunc(func(context actor.Context) {
 		switch msg := context.Message().(type) {
 		case *myMessage:
 			log.Printf("%v got message %d", context.Self(), msg.i)
 		}
-	}
-	log.Println("Round robin routing:")
-	props := actor.FromFunc(act).WithPoolRouter(routing.NewRoundRobinPool(5))
+	})
+	props := act.WithPoolRouter(routing.NewRoundRobinPool(5))
 	pid := actor.Spawn(props)
 	for i := 0; i < 10; i++ {
 		pid.Tell(&myMessage{i})
 	}
 	time.Sleep(1 * time.Second)
 	log.Println("Random routing:")
-	props = actor.FromFunc(act).WithPoolRouter(routing.NewRandomPool(5))
+	props = act.WithPoolRouter(routing.NewRandomPool(5))
 	pid = actor.Spawn(props)
 	for i := 0; i < 10; i++ {
 		pid.Tell(&myMessage{i})
 	}
 	time.Sleep(1 * time.Second)
 	log.Println("ConsistentHash routing:")
-	props = actor.FromFunc(act).WithPoolRouter(routing.NewConsistentHashPool(5))
+	props = act.WithPoolRouter(routing.NewConsistentHashPool(5))
 	pid = actor.Spawn(props)
 	for i := 0; i < 10; i++ {
 		pid.Tell(&myMessage{i})
