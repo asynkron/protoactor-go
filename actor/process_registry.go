91eae91d1e982240d49580bdfac32c95ad577560
rogeralsing <rogeralsing@gmail.com>
2016-11-25 12:18:22 +0100

    concurrent process registry

--- actor/process_registry.go
+++ actor/process_registry.go
@@ -2,22 +2,22 @@ package actor
 
 import (
 	"strconv"
-	"sync"
 	"sync/atomic"
+
+	cmap "github.com/orcaman/concurrent-map"
 )
 
 type ProcessRegistryValue struct {
 	Host           string
-	LocalPids      map[string]ActorRef //maybe this should be replaced with something lockfree like ctrie instead
+	LocalPids      cmap.ConcurrentMap
 	RemoteHandlers []HostResolver
 	SequenceID     uint64
-	rw             sync.RWMutex
 }
 
 var (
 	ProcessRegistry = &ProcessRegistryValue{
 		Host:           "nonhost",
-		LocalPids:      make(map[string]ActorRef),
+		LocalPids:      cmap.New(),
 		RemoteHandlers: make([]HostResolver, 0),
 	}
 )
@@ -26,6 +26,7 @@ type HostResolver func(*PID) (ActorRef, bool)
 
 func (pr *ProcessRegistryValue) RegisterHostResolver(handler HostResolver) {
 	pr.RemoteHandlers = append(pr.RemoteHandlers, handler)
+
 }
 
 func (pr *ProcessRegistryValue) getAutoId() string {
@@ -40,21 +41,16 @@ func (pr *ProcessRegistryValue) registerPID(actorRef ActorRef, id string) (*PID,
 		Id:   id,
 	}
 
-	pr.rw.Lock()
-	_, found := pr.LocalPids[pid.Id]
+	_, found := pr.LocalPids.Get(pid.Id)
 	if found {
-		pr.rw.Unlock()
 		return &pid, false
 	}
-	pr.LocalPids[pid.Id] = actorRef
-	pr.rw.Unlock()
+	pr.LocalPids.Set(pid.Id, actorRef)
 	return &pid, true
 }
 
 func (pr *ProcessRegistryValue) unregisterPID(pid *PID) {
-	pr.rw.Lock()
-	delete(pr.LocalPids, pid.Id)
-	pr.rw.Unlock()
+	pr.LocalPids.Remove(pid.Id)
 }
 
 func (pr *ProcessRegistryValue) fromPID(pid *PID) (ActorRef, bool) {
@@ -68,13 +64,10 @@ func (pr *ProcessRegistryValue) fromPID(pid *PID) (ActorRef, bool) {
 		//panic("Unknown host or node")
 		return deadLetter, false
 	}
-	pr.rw.RLock()
-	ref, ok := pr.LocalPids[pid.Id]
+	ref, ok := pr.LocalPids.Get(pid.Id)
 	if !ok {
 		//panic("Unknown PID")
-		pr.rw.RUnlock()
 		return deadLetter, false
 	}
-	pr.rw.RUnlock()
-	return ref, true
+	return ref.(ActorRef), true
 }
