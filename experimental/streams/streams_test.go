e58839e5ab73c45bc0ef8cf170b3e72900da7abd
rogeralsing <rogeralsing@gmail.com>
2016-11-28 21:01:35 +0100

    .

--- experimental/streams/streams_test.go
+++ experimental/streams/streams_test.go
@@ -8,7 +8,12 @@ import (
 
 func TestReceiveFromStream(t *testing.T) {
 	s := NewUntypedStream()
-	go s.PID().Tell("hello")
+	go func() {
+		s.PID().Tell("hello")
+		s.PID().Tell("you")
+	}()
 	res := <-s.C()
+	res2 := <-s.C()
 	assert.Equal(t, "hello", res.(string))
+	assert.Equal(t, "you", res2.(string))
 }
