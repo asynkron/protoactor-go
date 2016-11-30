5b0ce2db8d36de7edf273e28a40af592d8ccb896
rogeralsing <rogeralsing@gmail.com>
2016-11-30 16:22:24 +0100

    .

--- examples/spawnbenchmark/main.go
+++ examples/spawnbenchmark/main.go
@@ -7,28 +7,10 @@ import (
 	"github.com/AsynkronIT/gam/actor"
 )
 
-func skynet(c chan int, num int, size int, div int) {
-	if size == 1 {
-		c <- num
-		return
-	}
-
-	rc := make(chan int)
-	var sum int
-	for i := 0; i < div; i++ {
-		subNum := num + i*(size/div)
-		go skynet(rc, subNum, size/div, div)
-	}
-	for i := 0; i < div; i++ {
-		sum += <-rc
-	}
-	c <- sum
-}
-
 type request struct {
-	num  int64
-	size int64
-	div  int64
+	num  int
+	size int
+	div  int
 }
 
 var (
@@ -36,8 +18,8 @@ var (
 )
 
 type state struct {
-	sum     int64
-	replies int64
+	sum     int
+	replies int
 	replyTo *actor.PID
 }
 
@@ -47,12 +29,6 @@ func newState() actor.Actor {
 
 func (s *state) Receive(ctx actor.Context) {
 	switch msg := ctx.Message().(type) {
-	case int64:
-		s.sum += msg
-		s.replies--
-		if s.replies == 0 {
-			s.replyTo.Tell(s.sum)
-		}
 	case *request:
 		if msg.size == 1 {
 			ctx.Respond(msg.num)
@@ -61,14 +37,19 @@ func (s *state) Receive(ctx actor.Context) {
 
 		s.replies = msg.div
 		s.replyTo = ctx.Sender()
-		for i := int64(0); i < msg.div; i++ {
-			subNum := msg.num + i*(msg.size/msg.div)
-			child := ctx.Spawn(props)
-			child.Request(&request{
-				num:  subNum,
-				size: msg.size / msg.div,
-				div:  msg.div,
-			}, ctx.Self())
+		for i := 0; i < msg.div; i++ {
+			ctx.Spawn(props).
+				Request(&request{
+					num:  msg.num + i*(msg.size/msg.div),
+					size: msg.size / msg.div,
+					div:  msg.div,
+				}, ctx.Self())
+		}
+	case int:
+		s.sum += msg
+		s.replies--
+		if s.replies == 0 {
+			s.replyTo.Tell(s.sum)
 		}
 	}
 }
@@ -81,7 +62,7 @@ func main() {
 		size: 1000000,
 		div:  10,
 	}, 10*time.Second).Result()
-	result := res.(int64)
+	result := res.(int)
 
 	took := time.Since(start)
 	fmt.Printf("Result: %d in %d ms.\n", result, took.Nanoseconds()/1e6)
