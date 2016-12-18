59e9fbdad6a36b0143a708753a7a316332457545
rogeralsing <rogeralsing@gmail.com>
2016-12-18 19:07:34 +0100

    C# remoting

---
+++ languages/csharp/examples/remotebenchmark/Messages/build.bat
@@ -0,0 +1,2 @@
+﻿
+protoc Protos.proto -I. -I%GOPATH%\src --csharp_out=.   --csharp_opt=file_extension=.g.cs --grpc_out . --plugin=protoc-gen-grpc=..\..\..\packages\Grpc.Tools.1.0.1\tools\windows_x64\grpc_csharp_plugin.exe
---
+++ languages/csharp/src/GAM.Remoting/EndpointReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Utils;
+
+namespace GAM.Remoting
+{
+    public class EndpointReader : Remoting.RemotingBase
+    {
+        public override async Task<Unit> Receive(IAsyncStreamReader<MessageBatch> requestStream, ServerCallContext context)
+        {
+            await requestStream.ForEachAsync(batch =>
+            {
+                foreach (var envelope in batch.Envelopes)
+                {
+                    Console.WriteLine(envelope.ToString());
+                }
+               
+                return Actor.Done;
+            });
+            return new Unit();
+        }
+    }
+}
