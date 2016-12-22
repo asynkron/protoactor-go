e8beedf515b7358cd932cacc8d3fe32550ab0492
rogeralsing <rogeralsing@gmail.com>
2016-12-22 23:36:06 +0100

    removing pool dispatcher

--- actor/dispatcher.go
+++ actor/dispatcher.go
@@ -1,7 +1,5 @@
 package actor
 
-import "github.com/ivpusic/grpool"
-
 type Dispatcher interface {
 	Schedule(runner MailboxRunner)
 	Throughput() int
@@ -29,28 +27,4 @@ func NewDefaultDispatcher(throughput int) Dispatcher {
 	return &goroutineDispatcher{
 		throughput: throughput,
 	}
-}
-
-type poolDispatcher struct {
-	pool       *grpool.Pool
-	throughput int
-}
-
-func NewPoolDispatcher(workers int, queueSize int, throughput int) Dispatcher {
-	pool := grpool.NewPool(workers, queueSize)
-	d := &poolDispatcher{
-		pool:       pool,
-		throughput: throughput,
-	}
-	return d
-}
-
-func (d *poolDispatcher) Schedule(runner MailboxRunner) {
-	d.pool.JobQueue <- func() {
-		runner()
-	}
-}
-
-func (d *poolDispatcher) Throughput() int {
-	return d.throughput
 }
