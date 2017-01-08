5ba837192d291461e7718f14913adba879a0e4fd
rogeralsing <rogeralsing@gmail.com>
2017-01-08 19:52:12 +0100

    .

--- examples/cluster/seed/main.go
+++ examples/cluster/seed/main.go
@@ -4,17 +4,26 @@ import (
 	"log"
 
 	console "github.com/AsynkronIT/goconsole"
+	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/AsynkronIT/protoactor-go/cluster"
 	"github.com/AsynkronIT/protoactor-go/consul_cluster"
 	"github.com/AsynkronIT/protoactor-go/examples/cluster/shared"
+	"github.com/AsynkronIT/protoactor-go/remoting"
 )
 
 func main() {
+
+	//this node knows about Hello kind
+	remoting.Register("Hello", actor.FromProducer(func() actor.Actor {
+		return &shared.HelloActor{}
+	}))
+
 	cp, err := consul_cluster.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 	cluster.StartWithProvider("mycluster", "127.0.0.1:8080", cp)
+
 	hello := shared.GetHelloGrain("MyGrain")
 
 	res, err := hello.SayHello(&shared.HelloRequest{Name: "Roger"})
--- examples/cluster/shared/protos_protoactor.go
+++ examples/cluster/shared/protos_protoactor.go
@@ -20,7 +20,6 @@ package shared
 import errors "errors"
 import log "log"
 import actor "github.com/AsynkronIT/protoactor-go/actor"
-import remoting "github.com/AsynkronIT/protoactor-go/remoting"
 import cluster "github.com/AsynkronIT/protoactor-go/cluster"
 
 import proto "github.com/gogo/protobuf/proto"
@@ -306,10 +305,6 @@ func (a *HelloActor) Receive(ctx actor.Context) {
 }
 
 func init() {
-
-	remoting.Register("Hello", actor.FromProducer(func() actor.Actor {
-		return &HelloActor{}
-	}))
 
 }
 
