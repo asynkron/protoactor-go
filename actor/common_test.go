90db55d231dbd07dbdbc9fc15c371833fe619d78
Stuart Carnie <stuart.carnie@gmail.com>
2017-01-12 09:50:28 -0700

    dead code

--- actor/common_test.go
+++ actor/common_test.go
@@ -3,7 +3,6 @@ package actor
 import (
 	"io/ioutil"
 	"log"
-	"sync/atomic"
 	"time"
 
 	"github.com/stretchr/testify/mock"
@@ -24,43 +23,6 @@ func (inlineDispatcher) Schedule(runner MailboxRunner) {
 
 func (inlineDispatcher) Throughput() int {
 	return 1
-}
-
-const defaultTimeout = 10 * time.Millisecond
-
-type waiter struct {
-	c  int32
-	ch chan struct{}
-}
-
-func newWaiter(c int32) *waiter {
-	return &waiter{c: c, ch: make(chan struct{})}
-}
-
-func (w *waiter) Add(c int32) {
-	v := atomic.AddInt32(&w.c, c)
-	if v == 0 {
-		w.ch <- struct{}{}
-	} else if v < 0 {
-		panic("<0")
-	}
-}
-
-func (w *waiter) Done() {
-	w.Add(-1)
-}
-
-func (w *waiter) Wait() bool {
-	return w.WaitTimeout(defaultTimeout)
-}
-
-func (w *waiter) WaitTimeout(t time.Duration) bool {
-	select {
-	case <-w.ch:
-		return true
-	case <-time.After(t):
-		return false
-	}
 }
 
 type mockActor struct {
