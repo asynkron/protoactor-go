242e55f8922ba3f4ca2af1e32c9236ffea4ac2c4
rogeralsing <rogeralsing@gmail.com>
2017-01-12 08:58:23 +0100

    remoting messages

---
+++ remoting/messages.go
@@ -0,0 +1,22 @@
+package remoting
+
+import "github.com/AsynkronIT/protoactor-go/actor"
+
+type EndpointTerminated struct {
+	Address string
+}
+
+type remoteWatch struct {
+	Watcher *actor.PID
+	Watchee *actor.PID
+}
+
+type remoteUnwatch struct {
+	Watcher *actor.PID
+	Watchee *actor.PID
+}
+
+type remoteTerminate struct {
+	Watcher *actor.PID
+	Watchee *actor.PID
+}
