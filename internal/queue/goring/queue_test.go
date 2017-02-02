6e7e6687c3c7d212c12cffbb00715834630d3322
rogeralsing <rogeralsing@gmail.com>
2017-02-02 20:57:54 +0100

    consistency check goring

--- internal/queue/goring/queue_test.go
+++ internal/queue/goring/queue_test.go
@@ -2,7 +2,12 @@ package goring
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"runtime"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -48,4 +53,61 @@ func TestPushPopMany2(t *testing.T) {
 		assert.Equal(t, item, res)
 	}
 	assert.True(t, q.Empty())
+}
+
+func TestLfQueueConsistency(t *testing.T) {
+	max := 1000000
+	c := 100
+	var wg sync.WaitGroup
+	wg.Add(1)
+	q := New(10)
+	go func() {
+		i := 0
+		seen := make(map[string]string)
+		for {
+			r, ok := q.Pop()
+			if !ok {
+				runtime.Gosched()
+
+				continue
+			}
+			i++
+			s := r.(string)
+			_, present := seen[s]
+			if present {
+				log.Printf("item have already been seen %v", s)
+				t.FailNow()
+			}
+			seen[s] = s
+
+			if i == max {
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	for j := 0; j < c; j++ {
+		jj := j
+		cmax := max / c
+		go func() {
+			for i := 0; i < cmax; i++ {
+				if rand.Intn(10) == 0 {
+					time.Sleep(time.Duration(rand.Intn(1000)))
+				}
+				q.Push(fmt.Sprintf("%v %v", jj, i))
+			}
+		}()
+	}
+
+	wg.Wait()
+	time.Sleep(500 * time.Millisecond)
+	//queue should be empty
+	for i := 0; i < 100; i++ {
+		r, ok := q.Pop()
+		if ok {
+			log.Printf("unexpected result %+v", r)
+			t.FailNow()
+		}
+	}
 }
