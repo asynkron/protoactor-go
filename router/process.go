872eaf61e60377c7795b1b40dbf33abae7f730fa
Stuart Carnie <stuart.carnie@gmail.com>
2017-02-02 21:13:25 -0700

    fix: short-term fix to ensure router properly cleans up
    * intercepts Watch / Unwatch and manages it's own watcher list
    * sends Terminated to watchers when stopping

--- router/process.go
+++ router/process.go
@@ -1,12 +1,20 @@
 package router
 
-import "github.com/AsynkronIT/protoactor-go/actor"
+import (
+	"sync"
+	"sync/atomic"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+)
 
 // process serves as a proxy to the router implementation and forwards messages directly to the routee. This
 // optimization avoids serializing router messages through an actor
 type process struct {
-	router *actor.PID
-	state  Interface
+	router   *actor.PID
+	state    Interface
+	mu       sync.Mutex
+	watchers actor.PIDSet
+	stopping int32
 }
 
 func (ref *process) SendUserMessage(pid *actor.PID, message interface{}, sender *actor.PID) {
@@ -19,11 +27,46 @@ func (ref *process) SendUserMessage(pid *actor.PID, message interface{}, sender 
 }
 
 func (ref *process) SendSystemMessage(pid *actor.PID, message interface{}) {
-	r, _ := actor.ProcessRegistry.Get(ref.router)
-	r.SendSystemMessage(pid, message)
+	switch msg := message.(type) {
+	case *actor.Watch:
+		if atomic.LoadInt32(&ref.stopping) == 1 {
+			if r, ok := actor.ProcessRegistry.Get(msg.Watcher); ok {
+				r.SendSystemMessage(msg.Watcher, &actor.Terminated{Who: pid})
+			}
+			return
+		}
+		ref.mu.Lock()
+		ref.watchers.Add(msg.Watcher)
+		ref.mu.Unlock()
+
+	case *actor.Unwatch:
+		ref.mu.Lock()
+		ref.watchers.Remove(msg.Watcher)
+		ref.mu.Unlock()
+
+	case *actor.Stop:
+		term := &actor.Terminated{Who: pid}
+		ref.mu.Lock()
+		ref.watchers.ForEach(func(_ int, other actor.PID) {
+			if r, ok := actor.ProcessRegistry.Get(&other); ok {
+				r.SendSystemMessage(&other, term)
+			}
+		})
+		ref.mu.Unlock()
+
+	default:
+		r, _ := actor.ProcessRegistry.Get(ref.router)
+		r.SendSystemMessage(pid, message)
+
+	}
 }
 
 func (ref *process) Stop(pid *actor.PID) {
+	if atomic.SwapInt32(&ref.stopping, 1) == 1 {
+		return
+	}
+
+	ref.router.StopFuture().Wait()
 	actor.ProcessRegistry.Remove(pid)
 	ref.SendSystemMessage(pid, &actor.Stop{})
 }
