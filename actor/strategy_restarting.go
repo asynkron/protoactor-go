1e791173ad7be28bc57a27b91ccd1cf082352c73
rogeralsing <rogeralsing@gmail.com>
2017-02-06 00:58:45 +0100

    make restarting strategy use supervisor

--- actor/strategy_restarting.go
+++ actor/strategy_restarting.go
@@ -8,5 +8,5 @@ type restartingStrategy struct{}
 
 func (strategy *restartingStrategy) HandleFailure(supervisor Supervisor, child *PID, rs *RestartStatistics, reason interface{}, message interface{}) {
 	//always restart
-	child.sendSystemMessage(restartMessage)
+	supervisor.RestartChildren(child)
 }
