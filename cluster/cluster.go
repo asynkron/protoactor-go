196067f48e05d10d9e66e5b5ead8943f243df91a
rogeralsing <rogeralsing@gmail.com>
2017-03-19 23:47:36 +0100

    restructure

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,6 +1,9 @@
 package cluster
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/AsynkronIT/gonet"
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/AsynkronIT/protoactor-go/log"
@@ -26,4 +29,33 @@ func Start(clusterName, address string, provider ClusterProvider) {
 	subscribeMembershipActorToEventStream()
 	provider.RegisterMember(clusterName, h, p, kinds)
 	provider.MonitorMemberStatusChanges()
+}
+
+func getRandomActivator(kind string) string {
+
+	r := rand.Int()
+	members := getMembers(kind)
+	i := r % len(members)
+	member := members[i]
+	return member
+}
+
+//Get a PID to a virtual actor
+func Get(name string, kind string) (*actor.PID, error) {
+
+	req := &pidCacheRequest{
+		kind: kind,
+		name: name,
+	}
+
+	res, err := pidCacheActorPid.RequestFuture(req, 5*time.Second).Result()
+	if err != nil {
+		plog.Error("ActorPidRequest timed out", log.String("name", name), log.Error(err))
+		return nil, err
+	}
+	typed, ok := res.(*remote.ActorPidResponse)
+	if !ok {
+		plog.Error("ActorPidRequest returned incorrect response", log.String("name", name))
+	}
+	return typed.Pid, nil
 }
