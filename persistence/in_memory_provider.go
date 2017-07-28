be101aa239abdc48ebd8326952c735086b5e6f3d
RussellLuo <luopeng.he@gmail.com>
2017-07-28 23:14:19 +0800

    Refactor in-memory persistence provider to be concurrency-safe
    Fixed issue #154.

--- persistence/in_memory_provider.go
+++ persistence/in_memory_provider.go
@@ -1,24 +1,46 @@
 package persistence
 
-import "github.com/golang/protobuf/proto"
+import (
+	"sync"
 
-type snapshotEntry struct {
-	eventIndex int
+	"github.com/golang/protobuf/proto"
+)
+
+type entry struct {
+	eventIndex int // the event index right after snapshot
 	snapshot   proto.Message
+	events     []proto.Message
 }
 
 type InMemoryProvider struct {
 	snapshotInterval int
-	snapshots        map[string]*snapshotEntry  // actorName -> a snapshot entry
-	events           map[string][]proto.Message // actorName -> a list of events
+	mu               sync.RWMutex
+	store            map[string]*entry // actorName -> a persistence entry
 }
 
 func NewInMemoryProvider(snapshotInterval int) *InMemoryProvider {
 	return &InMemoryProvider{
 		snapshotInterval: snapshotInterval,
-		snapshots:        make(map[string]*snapshotEntry),
-		events:           make(map[string][]proto.Message),
+		store:            make(map[string]*entry),
 	}
+}
+
+// loadOrInit returns the existing entry for actorName if present.
+// Otherwise, it initializes and returns an empty entry.
+// The loaded result is true if the entry was loaded, false if initialized.
+func (provider *InMemoryProvider) loadOrInit(actorName string) (e *entry, loaded bool) {
+	provider.mu.RLock()
+	e, ok := provider.store[actorName]
+	provider.mu.RUnlock()
+
+	if !ok {
+		provider.mu.Lock()
+		e = &entry{}
+		provider.store[actorName] = e
+		provider.mu.Unlock()
+	}
+
+	return e, ok
 }
 
 func (provider *InMemoryProvider) Restart() {}
@@ -28,23 +50,27 @@ func (provider *InMemoryProvider) GetSnapshotInterval() int {
 }
 
 func (provider *InMemoryProvider) GetSnapshot(actorName string) (snapshot interface{}, eventIndex int, ok bool) {
-	entry, ok := provider.snapshots[actorName]
-	if !ok {
+	entry, loaded := provider.loadOrInit(actorName)
+	if !loaded || entry.snapshot == nil {
 		return nil, 0, false
 	}
 	return entry.snapshot, entry.eventIndex, true
 }
 
 func (provider *InMemoryProvider) PersistSnapshot(actorName string, eventIndex int, snapshot proto.Message) {
-	provider.snapshots[actorName] = &snapshotEntry{eventIndex: eventIndex, snapshot: snapshot}
+	entry, _ := provider.loadOrInit(actorName)
+	entry.eventIndex = eventIndex
+	entry.snapshot = snapshot
 }
 
 func (provider *InMemoryProvider) GetEvents(actorName string, eventIndexStart int, callback func(e interface{})) {
-	for _, e := range provider.events[actorName][eventIndexStart:] {
+	entry, _ := provider.loadOrInit(actorName)
+	for _, e := range entry.events[eventIndexStart:] {
 		callback(e)
 	}
 }
 
 func (provider *InMemoryProvider) PersistEvent(actorName string, eventIndex int, event proto.Message) {
-	provider.events[actorName] = append(provider.events[actorName], event)
+	entry, _ := provider.loadOrInit(actorName)
+	entry.events = append(entry.events, event)
 }
