2fd2db0533e6c22f5dda4e6d20c746fa11c0f48a
Potter Dai <potterdaica@gmail.com>
2017-09-30 03:56:00 -0400

    Add keyNameMap for better performance at termination.

--- cluster/partition_actor.go
+++ cluster/partition_actor.go
@@ -58,17 +58,19 @@ func partitionForKind(address, kind string) *actor.PID {
 func newPartitionActor(kind string) actor.Producer {
 	return func() actor.Actor {
 		return &partitionActor{
-			partition: make(map[string]*actor.PID),
-			kind:      kind,
-			counter:   &counter{},
+			partition:  make(map[string]*actor.PID),
+			keyNameMap: make(map[string]string),
+			kind:       kind,
+			counter:    &counter{},
 		}
 	}
 }
 
 type partitionActor struct {
-	partition map[string]*actor.PID //actor/grain name to PID
-	kind      string
-	counter   *counter
+	partition  map[string]*actor.PID //actor/grain name to PID
+	keyNameMap map[string]string     //actor/grain key to name
+	kind       string
+	counter    *counter
 }
 
 func (state *partitionActor) Receive(context actor.Context) {
@@ -140,6 +142,7 @@ func (state *partitionActor) spawn(msg *remote.ActorPidRequest, context actor.Co
 		case remote.ResponseStatusCodeOK:
 			pid = resp.Pid
 			state.partition[msg.Name] = pid
+			state.keyNameMap[pid.String()] = msg.Name
 			context.Watch(pid)
 			context.Respond(resp)
 			break
@@ -160,11 +163,10 @@ func (state *partitionActor) spawn(msg *remote.ActorPidRequest, context actor.Co
 
 func (state *partitionActor) terminated(msg *actor.Terminated) {
 	//one of the actors we manage died, remove it from the lookup
-	for actorID, pid := range state.partition {
-		if pid.Equal(msg.Who) {
-			delete(state.partition, actorID)
-			return
-		}
+	key := msg.Who.String()
+	if name, ok := state.keyNameMap[key]; ok {
+		delete(state.partition, name)
+		delete(state.keyNameMap, key)
 	}
 }
 
@@ -175,6 +177,7 @@ func (state *partitionActor) memberRejoined(msg *MemberRejoinedEvent) {
 		if pid.Address == msg.Name() {
 			//	log.Printf("[CLUSTER] Forgetting '%v' - '%v'", actorID, msg.Name())
 			delete(state.partition, actorID)
+			delete(state.keyNameMap, pid.String())
 		}
 	}
 }
@@ -186,6 +189,7 @@ func (state *partitionActor) memberLeft(msg *MemberLeftEvent) {
 		if pid.Address == msg.Name() {
 			//	log.Printf("[CLUSTER] Forgetting '%v' - '%v'", actorID, msg.Name())
 			delete(state.partition, actorID)
+			delete(state.keyNameMap, pid.String())
 		}
 	}
 }
@@ -209,10 +213,12 @@ func (state *partitionActor) transferOwnership(actorID string, address string, c
 	})
 	//we can safely delete this entry as the consisntent hash no longer points to us
 	delete(state.partition, actorID)
+	delete(state.keyNameMap, pid.String())
 	context.Unwatch(pid)
 }
 
 func (state *partitionActor) takeOwnership(msg *TakeOwnership, context actor.Context) {
 	state.partition[msg.Name] = msg.Pid
+	state.keyNameMap[msg.Pid.String()] = msg.Name
 	context.Watch(msg.Pid)
 }
