37abf37ad24a6a412817262c66915115102c86ed
Stuart Carnie <stuart.carnie@gmail.com>
2019-06-28 11:16:22 +1000

    fix: Data race in mpsc

--- internal/queue/mpsc/mpsc.go
+++ internal/queue/mpsc/mpsc.go
@@ -38,7 +38,7 @@ func (q *Queue) Push(x interface{}) {
 	prev := (*node)(atomic.SwapPointer((*unsafe.Pointer)(unsafe.Pointer(&q.head)), unsafe.Pointer(n)))
 
 	// release node to consumer
-	prev.next = n
+	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&prev.next)), unsafe.Pointer(n))
 }
 
 // Pop removes the item from the front of the queue or nil if the queue is empty
