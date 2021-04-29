dadd4f5de2adc08b97790de9fe0c2e5e985d153b
Roger Johansson <rogeralsing@gmail.com>
2021-04-29 18:25:20 +0200

    Add metrics outline

--- actor/metrics.go
+++ actor/metrics.go
@@ -5,8 +5,12 @@ import "github.com/AsynkronIT/protoactor-go/extensions"
 var extensionId = extensions.NextExtensionId()
 
 type Metrics struct {
+	enabled bool
 }
 
+func (m *Metrics) Enabled() bool {
+	return m.enabled
+}
 func (m *Metrics) Id() extensions.ExtensionId {
 	return extensionId
 }
@@ -14,3 +18,15 @@ func (m *Metrics) Id() extensions.ExtensionId {
 func NewMetrics() *Metrics {
 	return &Metrics{}
 }
+
+//func (m *Metrics) NewGauge() {
+//
+//}
+//
+//func (m *Metrics) NewCounter() {
+//
+//}
+//
+//func (m *Metrics) NewHistogram() {
+//
+//}
