25f2986de315371260c21c6d51d67f2e24f74776
Roger Johansson <rogeralsing@gmail.com>
2021-05-15 09:51:27 +0200

    more porting

--- cluster/partition-identity/placement_actor.go
+++ cluster/partition-identity/placement_actor.go
@@ -33,8 +33,48 @@ func (p *placementActor) Receive(ctx actor.Context) {
 	}
 }
 
+func (p *placementActor) onTerminated(msg *actor.Terminated, ctx actor.Context) {
+	found, key, meta := p.pidToMeta(msg.Who)
+
+	activationTerminated := &clustering.ActivationTerminated{
+		Pid:             msg.Who,
+		ClusterIdentity: meta.ID,
+	}
+	p.partitionManager.cluster.MemberList.BroadcastEvent(activationTerminated, true)
+
+	if found {
+		delete(p.actors, *key)
+	}
+}
+
 func (p *placementActor) onIdentityHandoverRequest(msg *clustering.IdentityHandoverRequest, ctx actor.Context) {
 
+	count := 0
+	response := &clustering.IdentityHandoverResponse{}
+	requestAddress := ctx.Sender().Address
+	rdv := clustering.NewRendezvousV2(msg.Members)
+	for identity, meta := range p.actors {
+		//who owns this identity according to the requesters memberlist?
+		ownerAddress := rdv.Get(identity)
+		//this identity is not owned by the requester
+		if ownerAddress != requestAddress {
+			continue
+		}
+		//_logger.LogDebug("Transfer {Identity} to {newOwnerAddress} -- {TopologyHash}", clusterIdentity, ownerAddress,
+		//msg.TopologyHash
+		//);
+
+		actorToHandOver := &clustering.Activation{
+			ClusterIdentity: meta.ID,
+			Pid:             meta.PID,
+		}
+
+		response.Actors = append(response.Actors, actorToHandOver)
+		count++
+	}
+
+	plog.Debug("Transferred ownership to other members", log.Int("count", count))
+	ctx.Respond(response)
 }
 
 func (p *placementActor) onActivationRequest(msg *clustering.ActivationRequest, ctx actor.Context) {
@@ -48,18 +88,4 @@ func (p *placementActor) pidToMeta(pid *actor.PID) (bool, *string, *GrainMeta) {
 		}
 	}
 	return false, nil, nil
-}
-
-func (p *placementActor) onTerminated(msg *actor.Terminated, ctx actor.Context) {
-	found, key, meta := p.pidToMeta(msg.Who)
-
-	activationTerminated := &clustering.ActivationTerminated{
-		Pid:             msg.Who,
-		ClusterIdentity: meta.ID,
-	}
-	p.partitionManager.cluster.MemberList.BroadcastEvent(activationTerminated, true)
-
-	if found {
-		delete(p.actors, *key)
-	}
 }
