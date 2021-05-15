6fd7e28d31bb74a29804a16f66294d20ec5a813b
Roger Johansson <rogeralsing@gmail.com>
2021-05-15 16:56:06 +0200

    more porting

---
+++ _examples/cluster-basic/node1/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AsynkronIT/goconsole"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/cluster"
+	"github.com/AsynkronIT/protoactor-go/cluster/automanaged"
+	"github.com/AsynkronIT/protoactor-go/cluster/partition"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"time"
+)
+
+func main() {
+	c := startNode()
+
+	fmt.Print("\nBoot other nodes and press Enter\n")
+	console.ReadLine()
+
+	c.Shutdown(true)
+}
+
+func startNode() *cluster.Cluster {
+	system := actor.NewActorSystem()
+
+	provider := automanaged.NewWithConfig(2*time.Second, 6331, "localhost:6330", "localhost:6331")
+	lookup := partition.New()
+	config := remote.Configure("localhost", 0)
+	clusterConfig := cluster.Configure("my-cluster", provider, lookup, config)
+	c := cluster.New(system, clusterConfig)
+	c.StartMember()
+
+	return c
+}
---
+++ _examples/cluster-basic/node2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AsynkronIT/protoactor-go/cluster/partition"
+	"time"
+
+	console "github.com/AsynkronIT/goconsole"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/cluster"
+	"github.com/AsynkronIT/protoactor-go/cluster/automanaged"
+	"github.com/AsynkronIT/protoactor-go/remote"
+)
+
+func main() {
+	cluster := startNode()
+
+	fmt.Print("\nBoot other nodes and press Enter\n")
+	console.ReadLine()
+
+	cluster.Shutdown(true)
+}
+
+func startNode() *cluster.Cluster {
+	system := actor.NewActorSystem()
+
+	provider := automanaged.NewWithConfig(2*time.Second, 6330, "localhost:6330", "localhost:6331")
+	lookup := partition.New()
+	config := remote.Configure("localhost", 0)
+
+	props := actor.PropsFromFunc(func(ctx actor.Context) {
+		switch _ := ctx.Message().(type) {
+		case *actor.Started:
+			fmt.Println("Started")
+			//case *shared.Noop:
+			//	fmt.Printf("Hello %v\n", msg.Who)
+		}
+	})
+	helloKind := cluster.NewKind("hello", props)
+	clusterConfig := cluster.Configure("my-cluster", provider, lookup, config, helloKind)
+	c := cluster.New(system, clusterConfig)
+
+	c.StartMember()
+	return c
+}
