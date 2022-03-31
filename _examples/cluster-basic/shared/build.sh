8244dcd45107184cdcd4bb363569eb86a16f0288
Roger Johansson <rogeralsing@gmail.com>
2022-03-31 17:00:59 +0200

    fixing protos

--- _examples/cluster-basic/shared/build.sh
+++ _examples/cluster-basic/shared/build.sh
@@ -1,2 +1,2 @@
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=. protos.proto
+protoc --go_out=. --go_opt=paths=source_relative --proto_path=. protos.proto
 
--- _examples/cluster-metrics/shared/build.sh
+++ _examples/cluster-metrics/shared/build.sh
@@ -1,2 +1,2 @@
-protoc -I=. -I=$GOPATH/src --gogoslick_out=. protos.proto 
-protoc -I=. -I=$GOPATH/src --gograin_out=. protos.proto 
+protoc --go_out=. --go_opt=paths=source_relative --proto_path=. protos.proto
+protoc -I=. -I=$GOPATH/src --gograinv2_out=. protos.proto
