de5d3f2c76c198dc34bcad7b273f5f18802eb457
Roger Johansson <rogeralsing@gmail.com>
2022-04-03 17:06:04 +0200

    cluster grain example

---
+++ _examples/cluster-grain/node1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	console "github.com/asynkron/goconsole"
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/asynkron/protoactor-go/cluster"
+	"github.com/asynkron/protoactor-go/cluster/clusterproviders/consul"
+	"github.com/asynkron/protoactor-go/cluster/identitylookup/disthash"
+	"github.com/asynkron/protoactor-go/remote"
+)
+
+func main() {
+	c := startNode()
+
+	fmt.Print("\nBoot other nodes and press Enter\n")
+	console.ReadLine()
+	pid := c.Get("abc", "hello")
+	fmt.Printf("Got pid %v", pid)
+	fmt.Println()
+	console.ReadLine()
+	c.Shutdown(true)
+}
+
+func startNode() *cluster.Cluster {
+	system := actor.NewActorSystem()
+
+	provider, _ := consul.New()
+	lookup := disthash.New()
+	config := remote.Configure("localhost", 0)
+	clusterConfig := cluster.Configure("my-cluster", provider, lookup, config)
+	c := cluster.New(system, clusterConfig)
+	c.StartMember()
+
+	return c
+}
