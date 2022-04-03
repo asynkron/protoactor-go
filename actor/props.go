4b9ee3c2668a302bec3181c0d30de3d72f34a218
Roger Johansson <rogeralsing@gmail.com>
2022-04-03 18:14:30 +0200

    Props options

--- actor/props.go
+++ actor/props.go
@@ -119,6 +119,9 @@ func (props *Props) spawn(actorSystem *ActorSystem, name string, parentContext S
 	return props.getSpawner()(actorSystem, name, props, parentContext)
 }
 
+//props options
+type PropsOption func(props *Props)
+
 // WithProducer assigns a actor producer to the props
 func (props *Props) WithProducer(p Producer) *Props {
 	props.producer = p
@@ -207,6 +210,96 @@ func (props *Props) WithSpawnMiddleware(middleware ...SpawnMiddleware) *Props {
 	})
 
 	return props
+}
+
+//all of the With* functions as PropsOption functions
+
+func WithProducer(p Producer) PropsOption {
+	return func(props *Props) {
+		props.producer = p
+	}
+}
+
+func WithDispatcher(dispatcher mailbox.Dispatcher) PropsOption {
+	return func(props *Props) {
+		props.dispatcher = dispatcher
+	}
+}
+
+func WithMailbox(mailbox mailbox.Producer) PropsOption {
+	return func(props *Props) {
+		props.mailboxProducer = mailbox
+	}
+}
+
+func WithContextDecorator(contextDecorator ...ContextDecorator) PropsOption {
+	return func(props *Props) {
+		props.contextDecorator = append(props.contextDecorator, contextDecorator...)
+
+		props.contextDecoratorChain = makeContextDecoratorChain(props.contextDecorator, func(ctx Context) Context {
+			return ctx
+		})
+	}
+}
+
+func WithGuardian(guardian SupervisorStrategy) PropsOption {
+	return func(props *Props) {
+		props.guardianStrategy = guardian
+	}
+}
+
+func WithSupervisor(supervisor SupervisorStrategy) PropsOption {
+	return func(props *Props) {
+		props.supervisionStrategy = supervisor
+	}
+}
+
+func WithReceiverMiddleware(middleware ...ReceiverMiddleware) PropsOption {
+	return func(props *Props) {
+		props.receiverMiddleware = append(props.receiverMiddleware, middleware...)
+
+		// Construct the receiver middleware chain with the final receiver at the end
+		props.receiverMiddlewareChain = makeReceiverMiddlewareChain(props.receiverMiddleware, func(ctx ReceiverContext, envelope *MessageEnvelope) {
+			ctx.Receive(envelope)
+		})
+	}
+}
+
+func WithSenderMiddleware(middleware ...SenderMiddleware) PropsOption {
+	return func(props *Props) {
+		props.senderMiddleware = append(props.senderMiddleware, middleware...)
+
+		// Construct the sender middleware chain with the final sender at the end
+		props.senderMiddlewareChain = makeSenderMiddlewareChain(props.senderMiddleware, func(sender SenderContext, target *PID, envelope *MessageEnvelope) {
+			target.sendUserMessage(sender.ActorSystem(), envelope)
+		})
+	}
+}
+
+func WithSpawnFunc(spawn SpawnFunc) PropsOption {
+	return func(props *Props) {
+		props.spawner = spawn
+	}
+}
+
+func WithFunc(f ReceiveFunc) PropsOption {
+	return func(props *Props) {
+		props.producer = func() Actor { return f }
+	}
+}
+
+func WithSpawnMiddleware(middleware ...SpawnMiddleware) PropsOption {
+	return func(props *Props) {
+		props.spawnMiddleware = append(props.spawnMiddleware, middleware...)
+
+		// Construct the spawner middleware chain with the final spawner at the end
+		props.spawnMiddlewareChain = makeSpawnMiddlewareChain(props.spawnMiddleware, func(actorSystem *ActorSystem, id string, props *Props, parentContext SpawnerContext) (pid *PID, e error) {
+			if props.spawner == nil {
+				return defaultSpawner(actorSystem, id, props, parentContext)
+			}
+			return props.spawner(actorSystem, id, props, parentContext)
+		})
+	}
 }
 
 // PropsFromProducer creates a props with the given actor producer assigned
