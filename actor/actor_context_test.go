f5705a354a0b653c9921bc2abbdb2edc81d4d644
Roger Johansson <rogeralsing@gmail.com>
2022-04-06 18:17:09 +0200

    add batch message support

--- actor/actor_context_test.go
+++ actor/actor_context_test.go
@@ -266,5 +266,43 @@ func TestActorContextAutoRespondMessage(t *testing.T) {
 	res, err := rootContext.RequestFuture(pid, msg, 1*time.Second).Result()
 	assert.NoError(t, err)
 	assert.IsType(t, &dummyResponse{}, res)
+}
 
+type dummyMessageBatch struct {
+	messages []any
+}
+
+func (d dummyMessageBatch) GetMessages() []any {
+	return d.messages
+}
+
+func TestActorReceivesEachMessageInAMessageBatch(t *testing.T) {
+
+	// each message in the batch
+	seenMessagesWg := sync.WaitGroup{}
+	seenMessagesWg.Add(10)
+
+	// the batch message itself
+	seenBatchMessageWg := sync.WaitGroup{}
+	seenBatchMessageWg.Add(1)
+
+	pid := rootContext.Spawn(PropsFromFunc(func(ctx Context) {
+		if _, ok := ctx.Message().(*DummyMessage); ok {
+			seenMessagesWg.Done()
+		}
+
+		if _, ok := ctx.Message().(*dummyMessageBatch); ok {
+			seenBatchMessageWg.Done()
+		}
+	}))
+
+	batch := &dummyMessageBatch{messages: make([]any, 10)}
+
+	for i := 0; i < 10; i++ {
+		batch.messages[i] = &DummyMessage{}
+	}
+
+	rootContext.Send(pid, batch)
+	seenMessagesWg.Wait()
+	seenBatchMessageWg.Wait()
 }
