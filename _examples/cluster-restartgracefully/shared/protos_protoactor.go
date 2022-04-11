f9cb7b7dc7cbc785c7927c1270f3d291c5b569ed
Roger Johansson <rogeralsing@gmail.com>
2022-04-11 13:57:45 +0200

    fix make file

--- _examples/cluster-restartgracefully/shared/protos_protoactor.go
+++ _examples/cluster-restartgracefully/shared/protos_protoactor.go
@@ -5,9 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"time"
-
 	"google.golang.org/protobuf/proto"
+	"time"
 
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/asynkron/protoactor-go/cluster"
@@ -56,7 +55,7 @@ func GetCalculatorKind(opts ...actor.PropsOption) *cluster.Kind {
 }
 
 // GetCalculatorKind instantiates a new cluster.Kind for Calculator
-func NewCalculatorKind(factory func() Calculator, timeout time.Duration, opts ...actor.PropsOption) *cluster.Kind {
+func NewCalculatorKind(factory func() Calculator, timeout time.Duration ,opts ...actor.PropsOption) *cluster.Kind {
 	xCalculatorFactory = factory
 	props := actor.PropsFromProducer(func() actor.Actor {
 		return &CalculatorActor{
@@ -75,12 +74,13 @@ type Calculator interface {
 	Add(*NumberRequest, cluster.GrainContext) (*CountResponse, error)
 	Subtract(*NumberRequest, cluster.GrainContext) (*CountResponse, error)
 	GetCurrent(*Void, cluster.GrainContext) (*CountResponse, error)
+	
 }
 
 // CalculatorGrainClient holds the base data for the CalculatorGrain
 type CalculatorGrainClient struct {
-	Identity string
-	cluster  *cluster.Cluster
+	Identity      string
+	cluster *cluster.Cluster
 }
 
 // Add requests the execution on to the cluster with CallOptions
@@ -161,6 +161,7 @@ func (g *CalculatorGrainClient) GetCurrent(r *Void, opts ...cluster.GrainCallOpt
 	}
 }
 
+
 // CalculatorActor represents the actor structure
 type CalculatorActor struct {
 	ctx     cluster.GrainContext
@@ -171,7 +172,7 @@ type CalculatorActor struct {
 // Receive ensures the lifecycle of the actor for the received message
 func (a *CalculatorActor) Receive(ctx actor.Context) {
 	switch msg := ctx.Message().(type) {
-	case *actor.Started: // pass
+	case *actor.Started: //pass
 	case *cluster.ClusterInit:
 		a.ctx = cluster.NewGrainContext(ctx, msg.Identity, msg.Cluster)
 		a.inner = xCalculatorFactory()
@@ -180,7 +181,7 @@ func (a *CalculatorActor) Receive(ctx actor.Context) {
 		if a.Timeout > 0 {
 			ctx.SetReceiveTimeout(a.Timeout)
 		}
-	case *actor.ReceiveTimeout:
+	case *actor.ReceiveTimeout:		
 		ctx.Poison(ctx.Self())
 	case *actor.Stopped:
 		a.inner.Terminate(a.ctx)
@@ -261,7 +262,7 @@ func (a *CalculatorActor) Receive(ctx actor.Context) {
 			}
 			resp := &cluster.GrainResponse{MessageData: bytes}
 			ctx.Respond(resp)
-
+		
 		}
 	default:
 		a.inner.ReceiveDefault(a.ctx)
