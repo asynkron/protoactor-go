3324b8caf3a54cf8dbe657dbd51d7cec17a89945
Kunduin <kunduin@outlook.com>
2023-01-21 12:10:55 +0800

    Add pubsub producer test build script #599

--- cluster/build.sh
+++ cluster/build.sh
@@ -2,4 +2,5 @@ protoc -I=../actor --go_out=. --go_opt=paths=source_relative --proto_path=. clus
 protoc -I=../actor --go_out=. --go_opt=paths=source_relative --proto_path=. gossip.proto
 protoc -I=../actor --go_out=. --go_opt=paths=source_relative --proto_path=. grain.proto
 protoc -I=../actor --go_out=. --go_opt=paths=source_relative --proto_path=. pubsub.proto
+protoc -I=../actor --go_out=. --go_opt=paths=source_relative --proto_path=. pubsub_test.proto
 
