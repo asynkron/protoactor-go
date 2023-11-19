284c6276cbfbefaf139d443d65e7cc62d2ae75de
Roger Johansson <roger@asynkron.se>
2023-11-19 14:26:42 +0100

    Start using slog

--- actor/middleware/logging.go
+++ actor/middleware/logging.go
@@ -1,17 +1,15 @@
 package middleware
 
 import (
-	"log"
-	"reflect"
-
 	"github.com/asynkron/protoactor-go/actor"
+	"log/slog"
 )
 
 // Logger is message middleware which logs messages before continuing to the next middleware.
 func Logger(next actor.ReceiverFunc) actor.ReceiverFunc {
 	fn := func(c actor.ReceiverContext, env *actor.MessageEnvelope) {
 		message := env.Message
-		log.Printf("%v got %v %+v", c.Self(), reflect.TypeOf(message), message)
+		c.Logger().Info("Actor got message", slog.Any("self", c.Self()), slog.Any("message", message))
 		next(c, env)
 	}
 
