6bb029eb9e2fadafda01d7cafbd8e91f6f5cb04d
Roger Johansson <roger@asynkron.se>
2023-12-20 18:06:16 +0100

    update build files

--- _examples/cluster-eventstream-broadcast/build.sh
+++ _examples/cluster-eventstream-broadcast/build.sh
@@ -1,2 +1,3 @@
 protoc --go_out=. --go_opt=paths=source_relative --proto_path=. protos.proto
-protoc -I=. -I=$GOPATH/src --gograinv2_out=. protos.proto
+protoc --go_out=. --go_opt=paths=source_relative --plugin=$GOPATH/bin/protoc-gen-go-grain --go-grain_out=. --go-grain_opt=paths=source_relative protos.proto
+
