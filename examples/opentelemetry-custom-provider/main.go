7e99a5345ebc8e96391fceed0543856e50c125ed
evilolipop <decai.wang@foxmail.com>
2024-01-17 18:18:22 +0800

    chore: comment out example because of sdk changed in opentelemetry-go

--- examples/opentelemetry-custom-provider/main.go
+++ examples/opentelemetry-custom-provider/main.go
@@ -1,69 +1,52 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"time"
+// type (
+// 	hello      struct{ Who string }
+// 	helloActor struct{}
+// )
 
-	console "github.com/asynkron/goconsole"
-	"github.com/asynkron/protoactor-go/actor"
-	"go.opentelemetry.io/otel"
+// func (state *helloActor) Receive(context actor.Context) {
+// 	switch msg := context.Message().(type) {
+// 	case *hello:
+// 		fmt.Printf("Hello %s\n", msg.Who)
+// 	}
+// }
 
-	stdout "go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	"go.opentelemetry.io/otel/metric"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
-)
+// func main() {
+// 	ctx := context.Background()
+// 	provider := stdoutExporter(ctx)
+// 	defer func() {
+// 		if err := provider.(*controller.Controller).Stop(ctx); err != nil {
+// 			log.Fatalf("could not stop push controller: %v", err)
+// 		}
+// 	}()
 
-type (
-	hello      struct{ Who string }
-	helloActor struct{}
-)
+// 	config := actor.Configure(actor.WithMetricProviders(provider))
+// 	system := actor.NewActorSystemWithConfig(config)
+// 	props := actor.PropsFromProducer(func() actor.Actor {
+// 		return &helloActor{}
+// 	})
 
-func (state *helloActor) Receive(context actor.Context) {
-	switch msg := context.Message().(type) {
-	case *hello:
-		fmt.Printf("Hello %s\n", msg.Who)
-	}
-}
+// 	pid := system.Root.Spawn(props)
+// 	system.Root.Request(pid, &hello{Who: "Stdout Exporter"})
+// 	time.Sleep(100 * time.Millisecond)
+// 	_, _ = console.ReadLine()
+// }
 
-func main() {
-	ctx := context.Background()
-	provider := stdoutExporter(ctx)
-	defer func() {
-		if err := provider.(*controller.Controller).Stop(ctx); err != nil {
-			log.Fatalf("could not stop push controller: %v", err)
-		}
-	}()
+// func stdoutExporter(ctx context.Context) metric.MeterProvider {
+// 	exporter, _ := stdout.New(stdout.WithPrettyPrint())
+// 	provider := controller.New(
+// 		processor.NewFactory(
+// 			simple.NewWithInexpensiveDistribution(),
+// 			exporter,
+// 		),
+// 		controller.WithExporter(exporter),
+// 	)
 
-	config := actor.Configure(actor.WithMetricProviders(provider))
-	system := actor.NewActorSystemWithConfig(config)
-	props := actor.PropsFromProducer(func() actor.Actor {
-		return &helloActor{}
-	})
+// 	if err := provider.Start(ctx); err != nil {
+// 		log.Fatalf("could not start push controller: %v", err)
+// 	}
+// 	otel.SetMeterProvider(provider)
 
-	pid := system.Root.Spawn(props)
-	system.Root.Request(pid, &hello{Who: "Stdout Exporter"})
-	time.Sleep(100 * time.Millisecond)
-	_, _ = console.ReadLine()
-}
-
-func stdoutExporter(ctx context.Context) metric.MeterProvider {
-	exporter, _ := stdout.New(stdout.WithPrettyPrint())
-	provider := controller.New(
-		processor.NewFactory(
-			simple.NewWithInexpensiveDistribution(),
-			exporter,
-		),
-		controller.WithExporter(exporter),
-	)
-
-	if err := provider.Start(ctx); err != nil {
-		log.Fatalf("could not start push controller: %v", err)
-	}
-	otel.SetMeterProvider(provider)
-
-	return provider
-}
+// 	return provider
+// }
