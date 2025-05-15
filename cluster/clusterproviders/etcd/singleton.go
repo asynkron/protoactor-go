70c71420be7d762c7b10723d798cbbdd224969f3
pipiaha <393818655@qq.com>
2025-05-15 11:49:05 +0800

    feat: etcd singleton

---
+++ cluster/clusterproviders/etcd/singleton.go
@@ -0,0 +1,71 @@
+package etcd
+
+import (
+	"github.com/asynkron/protoactor-go/actor"
+	"sync"
+)
+
+// RoleType 表示节点在集群中的角色
+type RoleType int
+
+const (
+	Follower RoleType = iota
+	Leader
+)
+
+func (r RoleType) String() string {
+	switch r {
+	case Leader:
+		return "Leader"
+	default:
+		return "Follower"
+	}
+}
+
+// SingletonScheduler 管理在领导节点上运行的单例 actor
+type SingletonScheduler struct {
+	sync.Mutex
+	root  *actor.RootContext
+	props []*actor.Props
+	pids  []*actor.PID
+}
+
+func NewSingletonScheduler(rc *actor.RootContext) *SingletonScheduler {
+	return &SingletonScheduler{root: rc}
+}
+
+func (s *SingletonScheduler) FromFunc(f actor.ReceiveFunc) *SingletonScheduler {
+	s.Lock()
+	defer s.Unlock()
+	s.props = append(s.props, actor.PropsFromFunc(f))
+	return s
+}
+
+func (s *SingletonScheduler) FromProducer(f actor.Producer) *SingletonScheduler {
+	s.Lock()
+	defer s.Unlock()
+	s.props = append(s.props, actor.PropsFromProducer(f))
+	return s
+}
+
+func (s *SingletonScheduler) OnRoleChanged(rt RoleType) {
+	s.Lock()
+	defer s.Unlock()
+	if rt == Follower {
+		if len(s.pids) > 0 {
+			s.root.Logger().Info("I am follower, poison singleton actors")
+			for _, pid := range s.pids {
+				s.root.Poison(pid)
+			}
+			s.pids = nil
+		}
+	} else if rt == Leader {
+		if len(s.props) > 0 {
+			s.root.Logger().Info("I am leader now, start singleton actors")
+			s.pids = make([]*actor.PID, len(s.props))
+			for i, p := range s.props {
+				s.pids[i] = s.root.Spawn(p)
+			}
+		}
+	}
+}
