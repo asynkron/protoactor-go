f6ec66d20ab89e4701911fcae5d96d3a6ccdc0ca
Roger Johansson <roger@asynkron.se>
2025-08-25 09:11:02 +0200

    Handle missing proto types during deserialization

--- remote/proto_serializer.go
+++ remote/proto_serializer.go
@@ -14,6 +14,7 @@ func newProtoSerializer() *protoSerializer {
 	return &protoSerializer{}
 }
 
+// Serialize converts a protobuf message into its binary form.
 func (p *protoSerializer) Serialize(msg interface{}) ([]byte, error) {
 	if message, ok := msg.(proto.Message); ok {
 		bytes, err := proto.Marshal(message)
@@ -26,15 +27,20 @@ func (p *protoSerializer) Serialize(msg interface{}) ([]byte, error) {
 	return nil, fmt.Errorf("msg must be proto.Message")
 }
 
+// Deserialize creates a protobuf message of the given type and fills it with the provided bytes.
 func (p *protoSerializer) Deserialize(typeName string, bytes []byte) (interface{}, error) {
-	n, _ := protoregistry.GlobalTypes.FindMessageByName(protoreflect.FullName(typeName))
+	mt, err := protoregistry.GlobalTypes.FindMessageByName(protoreflect.FullName(typeName))
+	if err != nil {
+		return nil, err
+	}
 
-	pm := n.New().Interface()
+	pm := mt.New().Interface()
 
-	err := proto.Unmarshal(bytes, pm)
+	err = proto.Unmarshal(bytes, pm)
 	return pm, err
 }
 
+// GetTypeName returns the fully qualified name of a protobuf message.
 func (protoSerializer) GetTypeName(msg interface{}) (string, error) {
 	if message, ok := msg.(proto.Message); ok {
 		typeName := proto.MessageName(message)
